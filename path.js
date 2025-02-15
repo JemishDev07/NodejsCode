7edc08b336717137389e2de01f554d64ec32de7e
JemishDev07 <jemishdev@gmail.com>
2025-02-15 14:32:56 -0500

    add code

---
+++ path.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+console.log(__dirname);
+console.log(__filename);
+
+const Newpath = path.join("jemish", "music", "songs.txt");
+console.log(Newpath);
+
+const Parse = path.parse(Newpath);
+const resolve = path.resolve(Newpath);
+const extname = path.extname(Newpath);
+const basename = path.basename(Newpath);
+const pathsep = path.sep;
+
+console.log({ Parse, resolve, extname, pathsep, basename });
